581c580a4783ae1f9fad2eb30e40b7cf73e105ff
Anna Marfel <anna.marfel@gmail.com>
2018-02-25 00:30:47 +0300

    not already done

--- src/index.js
+++ src/index.js
@@ -1,31 +1,54 @@
 class Sorter {
-  constructor() {
-    // your implementation
-  }
+    constructor() {
+        this.wrapper = [];
+    }
 
-  add(element) {
-    // your implementation
-  }
+    add(element) {
+        return this.wrapper.push(element);
+    }
 
-  at(index) {
-    // your implementation
-  }
+    at(index) {
+        return this.wrapper[index];
+    }
 
-  get length() {
-    // your implementation
-  }
+    get length() {
+        return this.wrapper.length;
 
-  toArray() {
-    // your implementation
-  }
+    }
 
-  sort(indices) {
-    // your implementation
-  }
+    toArray() {
+        return this.wrapper;
+    }
 
-  setComparator(compareFunction) {
-    // your implementation
-  }
+    sort(indices) {
+        let sortedArray = [];
+        for (let i = 0; i < indices.length; i++) {
+            sortedArray.push(this.wrapper[indices[i]]);
+        }
+        sortedArray.sort(
+            function (A, B) {
+                return A - B;
+            }
+        );
+        return sortedArray;
+    }
+
+    setComparator(compareFunction) {
+        return  this.wrapper.sort(compareFunction);
+    }
 }
 
 module.exports = Sorter;
+
+// it('3', () => {
+//     sorter.add(7);
+//     sorter.add(6);
+//     sorter.add(5);
+//     sorter.sort([0, 1]);
+//
+//     assert.deepEqual(sorter.toArray(), [6, 7, 5]);
+//     assert.deepEqual(sorter.length, 3);
+//     assert.deepEqual(sorter.at(0), 6);
+//     assert.deepEqual(sorter.at(1), 7);
+//     assert.deepEqual(sorter.at(2), 5);
+// });
